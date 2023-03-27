ff82c36579417fa47c75fecee9f324bc260a5b00
nzcer <ningzhicheng1215@qq.com>
2023-03-27 19:28:45 +0800

    feat: 腾讯后台开发方向

---
+++ src/main/java/tencent/d20230326/T3.java
@@ -0,0 +1,21 @@
+package tencent.d20230326;
+
+/**
+ * @project: x-interview
+ * @ClassName: T3
+ * @author: nzcer
+ * @creat: 2023/3/26 20:46
+ * @description:
+ * in:
+ * 2
+ * 1 10
+ * 1 0
+ * out:
+ * 10
+ */
+public class T3 {
+
+    public static void main(String[] args) {
+
+    }
+}
